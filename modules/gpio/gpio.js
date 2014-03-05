78af56090d5ca6c156e5e3a058abfb7d346298fa
Fabian Bühler <fabian.b89@gmail.com>
2014-03-05 17:00:39 +0100

    GPIO Kontrolle mit Pins und JQuery Effekten II

--- modules/gpio/gpio.js
+++ modules/gpio/gpio.js
@@ -1,31 +1,52 @@
 var assert = require("assert");
-
+var RaspberryPinInfo = require('./RaspberryPinInfo.json');
 
 var gpio = {};
 var i = 0;
+
+function PinStore() {
+    this.pins = {};
+}
+
+PinStore.prototype.add = function (pin, callback) {
+    pin._id = i++;
+    pin.State = new Boolean(false);
+
+    this.pins[pin._id] = pin;
+    setImmediate(callback, null);
+};
 
 module.exports = function(options, imports, register) {
     assert(imports.server, "Package 'server' is required");
 
     var io = imports.server.io;
 
+    //var gpio = require('rpi-gpio');
+    var pin_state = new Boolean(false);
+
+    var PinStore = new PinStore();
+    for (pin in RaspberryPinInfo)
+    {
+        //PinStore.add({pin:pin},function(err){});
+    }
+
     io.sockets.on('connection', function(socket) {
 
         socket.emit('hello', { hello: 'Hallo GPIOs' });
 
-        var state = new Boolean(false);
-        var states = {};
-
-        socket.on('setPin', function(pin) {
-            state = !state;
+        socket.on('setPin', function(pin_id) {
 
             /*
+            gpio.setup(pin_id, gpio.DIR_OUT, write);
+            pin_state = !pin_state;
 
-            Pinswitch hier her
-
+            function write() {
+                gpio.write(pin_id, pin_state, function(err) {
+                    if (err) throw err;
+                    console.log('Written to Pin: ' + pin_id + ' State: ' + pin_state);
+                });
+            }
             */
-
-            console.log('change Pin: ' + pin);
 
             var data = {
                 id : pin,
--- www/templates/gpio.tpl
+++ www/templates/gpio.tpl
@@ -2,19 +2,19 @@
 
 <div class="row">
 
-<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="1">
+<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="13">
     <div data-effect="bounce" class="effect-button bounce-button"><i class="fa fa-star"></i>GPIO 1</div>
 </div>
 
 
-<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="2">
+<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="15">
     <div data-effect="pulse" class="effect-button bounce-button"><i class="fa fa-star"></i>GPIO 2</div>
 </div>
 
-<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="3">
+<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="16">
     <div data-effect="flash" class="effect-button bounce-button"><i class="fa fa-star"></i>GPIO 3</div>
 </div>
 
-<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="4">
+<div class="col-lg-2 col-md-4 col-sm-4 col-xs-6" id="18">
     <div data-effect="shake" class="effect-button bounce-button"><i class="fa fa-star"></i>GPIO 4</div>
 </div>
