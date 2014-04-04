378536cf14436f48b90662f452535f68469444a9
Fabian Bühler <fabian.b89@gmail.com>
2014-04-04 12:53:58 +0200

    SPI implementiert

---
+++ modules/pi-spi/pi-spi.js
@@ -0,0 +1,34 @@
+var assert = require("assert");
+SPI = require('pi-spi');
+
+// options - enthält die in der config.js festgelegten Variablen
+// imports - enthält alle in der package.json mit 'consumes' referenzierten Module
+// register - Mithilfe register kann das komplette Modul oder nur Teilfunktionen verfügbar gemacht werden
+module.exports = function(options, imports, register) {
+    assert(imports.module1, "Package 'module1' is required");   // Module1 auf Verfügbarkeit prüfen
+    assert(options.port, "Option 'port' is required");          // Option 'port' auf Verfügbarkeit prüfen
+
+    var spi = SPI.initialize("/dev/spidev0.0"),
+        test = Buffer("Hello, World!");
+
+    // reads and writes simultaneously
+    spi.transfer(test, test.length, function (e,d) {
+        if (e) console.error(e);
+        else console.log("Got \""+d.toString()+"\" back.");
+
+        if (test.toString() === d.toString()) {
+            console.log(msg);
+        } else {
+            // NOTE: this will likely happen unless MISO is jumpered to MOSI
+            console.warn(msg);
+            process.exit(-2);
+        }
+    });
+
+    // Modul freigeben
+    register(null, {
+        "spi" : spi
+    });
+}
+
+
