3c821163f052a948088267e7e97a42011b641c85
Fabian Bühler <fabian.b89@gmail.com>
2014-04-25 17:14:55 +0200

    Events Prototyp hinzugefügt

--- www/static/scripts/plugins/events/routes/EVIndexRoute.js
+++ www/static/scripts/plugins/events/routes/EVIndexRoute.js
@@ -3,17 +3,17 @@ define(
     function() {
         return Ember.Route.extend({
 
-            model: function() {
-                return new Ember.RSVP.Promise(function(resolve) {
-                    App.io.on('main/events/list', function(events) {
-                        events = JSON.parse(events);
-                        resolve({'events': events });
-                    });
-                    App.io.emit('main/events/list');
-                }, 3000);
+            beforeModel: function(){
+                var self = this;
+
+                App.io.on('main/events/list', function(events) {
+                    events = JSON.parse(events);
+                    self.controller.set('events', events);
+                });
             },
-            setupController: function(controller, model) {
-                controller.set('events', model.events);
+
+            model: function() {
+                App.io.emit('main/events/list');
             }
         });
     })
