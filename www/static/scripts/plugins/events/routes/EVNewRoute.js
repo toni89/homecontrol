8eb6ed33410c12c224f506ed08aa6d0a20f902d0
Fabian Bühler <fabian.b89@gmail.com>
2014-04-29 17:07:01 +0200

    Back to the roots

--- www/static/scripts/plugins/events/routes/EVNewRoute.js
+++ www/static/scripts/plugins/events/routes/EVNewRoute.js
@@ -9,8 +9,15 @@ define(
                 submit: function() {
                     var self = this;
 
-                    var defaultC = this.controller.get('defaultConfig');
-                    
+                    var name = this.controller.get('defaultConfig').name;
+                    var description = this.controller.get('defaultConfig').description;
+                    var repeat_daily = this.controller.get('defaultConfig').repeat_daily;
+
+                    var start_mm = this.controller.get('defaultConfig').start_mm;
+                    var start_hh = this.controller.get('defaultConfig').start_hh;
+                    var end_mm = this.controller.get('defaultConfig').end_mm;
+                    var end_hh = this.controller.get('defaultConfig').end_hh;
+
                     if(name != '') {
                         App.io.on('eventobject saved', function(socket) {
                             console.log('eventobject PING zurück ' + socket);
@@ -18,13 +25,13 @@ define(
                         });
 
                         App.io.emit('events/createEvent', {
-                            name: defaultC.name,
-                            description: defaultC.description,
-                            repeat_daily: defaultC.repeat_daily,
-                            start_mm: defaultC.start_mm,
-                            start_hh: defaultC.start_hh,
-                            end_mm: defaultC.end_mm,
-                            end_hh: defaultC.end_hh});
+                            name: name,
+                            description: description,
+                            repeat_daily: repeat_daily,
+                            start_mm: start_mm,
+                            start_hh: start_hh,
+                            end_mm: end_mm,
+                            end_hh: end_hh});
                     }
 
                 }
