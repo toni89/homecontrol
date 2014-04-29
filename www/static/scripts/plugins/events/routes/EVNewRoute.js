9c94b5950899020d0ca03e97cf1470f6069297db
Fabian Bühler <fabian.b89@gmail.com>
2014-04-29 17:30:37 +0200

    Index Route ergänzt

--- www/static/scripts/plugins/events/routes/EVNewRoute.js
+++ www/static/scripts/plugins/events/routes/EVNewRoute.js
@@ -3,7 +3,19 @@ define(
     function() {
         return Ember.Route.extend({
 
+            model: function() {
+                return new Ember.RSVP.Promise(function(resolve) {
+                    App.io.on('main/devices/list', function(devices) {
+                        var devices = JSON.parse(devices);
+                        resolve({'devices': devices });
+                    });
+                    App.io.emit('main/devices/list');
+                }, 3000);
+            },
 
+            setupController: function(controller, model) {
+                controller.set('devices', model.devices);
+            },
 
             actions: {
                 submit: function() {
