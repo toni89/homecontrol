d0d1f97b106c2cab4b69a090f79b5fabc7edf6f4
Fabian Bühler <fabian.b89@gmail.com>
2014-04-29 16:36:35 +0200

    Simplyfy events route

--- www/static/scripts/plugins/events/routes/EVNewRoute.js
+++ www/static/scripts/plugins/events/routes/EVNewRoute.js
@@ -3,19 +3,14 @@ define(
     function() {
         return Ember.Route.extend({
 
+
+
             actions: {
                 submit: function() {
                     var self = this;
 
-                    var name = this.controller.get('defaultConfig').name;
-                    var description = this.controller.get('defaultConfig').description;
-                    var repeat_daily = this.controller.get('defaultConfig').repeat_daily;
-
-                    var start_mm = this.controller.get('defaultConfig').start_mm;
-                    var start_hh = this.controller.get('defaultConfig').start_hh;
-                    var end_mm = this.controller.get('defaultConfig').end_mm;
-                    var end_hh = this.controller.get('defaultConfig').end_hh;
-
+                    var defaultC = this.controller.get('defaultConfig');
+                    
                     if(name != '') {
                         App.io.on('eventobject saved', function(socket) {
                             console.log('eventobject PING zurück ' + socket);
@@ -23,13 +18,13 @@ define(
                         });
 
                         App.io.emit('events/createEvent', {
-                            name: name,
-                            description: description,
-                            repeat_daily: repeat_daily,
-                            start_mm: start_mm,
-                            start_hh: start_hh,
-                            end_mm: end_mm,
-                            end_hh: end_hh});
+                            name: defaultC.name,
+                            description: defaultC.description,
+                            repeat_daily: defaultC.repeat_daily,
+                            start_mm: defaultC.start_mm,
+                            start_hh: defaultC.start_hh,
+                            end_mm: defaultC.end_mm,
+                            end_hh: defaultC.end_hh});
                     }
 
                 }
